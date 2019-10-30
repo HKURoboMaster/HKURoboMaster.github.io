8c57017a1c08fa98b3c96c34bc6a65c363c952f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-30 18:40:37 +0000

    Deploy HKURoboMaster/HKURoboMaster.github.io to github.com/HKURoboMaster/HKURoboMaster.github.io.git:master

---
+++ precache-manifest.7666f7a736f5bdbfb1899f109a47be33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d70f6411180c40502f3cc711c00f47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa60f73b7c84c1f527ef",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "784c637f3e611dc91376",
+    "url": "/static/js/2.caa8ff9f.chunk.js"
+  },
+  {
+    "revision": "aa60f73b7c84c1f527ef",
+    "url": "/static/js/main.68596f0b.chunk.js"
+  },
+  {
+    "revision": "ae970b72d1121cf53cb6",
+    "url": "/static/js/runtime-main.3664b334.js"
+  },
+  {
+    "revision": "cf7f8340e9e7c580912351750073d8aa",
+    "url": "/static/media/AI_infantry.cf7f8340.png"
+  },
+  {
+    "revision": "9aa36be46d9706a74d4e47b413a80e27",
+    "url": "/static/media/logo.9aa36be4.png"
+  }
+]);
